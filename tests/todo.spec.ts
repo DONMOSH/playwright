59f36876758d48880afac46e7c6a888c9a73058a
DONMOSH <bennett.okle.anim@gmail.com>
2025-01-27 18:48:03 +0000

    manual tests

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,15 +1,40 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('active and completed filters', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/#/');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('water the plants');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('feed the dog');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-  await page.getByRole('listitem').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
-  await page.getByRole('link', { name: 'Active' }).click();
-  await expect(page.getByTestId('todo-title')).toContainText('feed the dog');
-  await page.getByRole('link', { name: 'Completed' }).click();
-  await expect(page.getByTestId('todo-title')).toContainText('water the plants');
+test.describe("New Todo", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("");
+  });
+
+  test("active and completed filters", async ({ page }) => {
+    const todoInput = page.getByRole("textbox", {
+      name: "What needs to be done?",
+    });
+
+    await todoInput.click();
+    await todoInput.fill("water the plants");
+    await todoInput.press("Enter");
+    await todoInput.fill("feed the dog");
+    await todoInput.press("Enter");
+    await page
+      .getByRole("listitem")
+      .filter({ hasText: "water the plants" })
+      .getByLabel("Toggle Todo")
+      .check();
+    await page.getByRole("link", { name: "Active" }).click();
+    await expect(page.getByTestId("todo-title")).toContainText("feed the dog");
+    await page.getByRole("link", { name: "Completed" }).click();
+    await expect(page.getByTestId("todo-title")).toContainText(
+      "water the plants"
+    );
+  });
+
+  test("text field is cleared when item is added", async ({ page }) => {
+    const todoInput = page.getByRole("textbox", {
+      name: "What needs to be done?",
+    });
+
+    await todoInput.fill("water the plants");
+    await todoInput.press("Enter");
+    await expect(todoInput).toBeEmpty();
+  });
 });
